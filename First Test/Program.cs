0a5daae25a12ea447a526312f01620c895dff792
K9042 <K9042@student.jamk.fi>
2017-01-19 13:14:14 +0200

    Salute corrected

--- First Test/Program.cs
+++ First Test/Program.cs
@@ -10,7 +10,7 @@ namespace First_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("Hello GitHubbi!");
         }
     }
 }
